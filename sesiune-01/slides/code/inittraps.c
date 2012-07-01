23d8a684972bab942aacad98ace1b5f5fe019b62
Irina Presa <irina.presa@gmail.com>
2012-07-01 17:40:19 +0300

    booting: almost ready

---
+++ sesiune-01/slides/code/inittraps.c
@@ -0,0 +1,18 @@
+set_trap_gate(0,&divide_error);
+set_intr_gate(1,&debug);
+set_intr_gate(2,&nmi);
+set_system_intr_gate(3, &int3);
+set_system_gate(4,&overflow);
+set_trap_gate(5,&bounds);
+set_trap_gate(6,&invalid_op);
+set_trap_gate(7,&device_not_available);
+set_task_gate(8,GDT_ENTRY_DOUBLEFAULT_TSS);
+set_trap_gate(9,&coprocessor_segment_overrun);
+set_trap_gate(10,&invalid_TSS);
+set_trap_gate(11,&segment_not_present);
+set_trap_gate(12,&stack_segment);
+set_trap_gate(13,&general_protection);
+set_intr_gate(14,&page_fault);
+set_trap_gate(15,&spurious_interrupt_bug);
+set_trap_gate(16,&coprocessor_error);
+set_trap_gate(17,&alignment_check);
