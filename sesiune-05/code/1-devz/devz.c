8ce580fb07c514bf6779f4739565aa504711e229
Daniel Baluta <daniel.baluta@gmail.com>
2012-07-06 08:43:10 +0300

    session 05: add devz tasks

--- sesiune-05/code/1-devz/devz.c
+++ sesiune-05/code/1-devz/devz.c
@@ -13,14 +13,30 @@
 #define MODULE_VERS "1.0"
 #define MODULE_NAME "devz"
 
+#define Z_MAJOR 42
+#define Z_MINOR 0
+
+
+/**
+ * define file operations struct to implement
+ * open and read operations
+ */
 
 static int __init devz_init(void)
 {
+	/* register_chrdev_region */
+	
+	/* init and add the cdev structure - cdev_init, cdev_add */
+	
+	
 	return 0;
 }
 
 static void __exit devz_exit(void)
 {
+	/* delete cdev structure - cdev_del */
+	
+	/* unregister_chrdev_region */
 }
 
 module_init(devz_init);
